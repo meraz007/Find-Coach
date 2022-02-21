db25de4860e142f4306d574d56f3fc4677143cf6
saiful Islam <saifulmeraz897@gmail.com>
2022-02-22 00:00:52 +0600

    adding base

--- src/components/Coaches/CoachesList.vue
+++ src/components/Coaches/CoachesList.vue
@@ -1,15 +1,21 @@
 <template>
     <section>FILTER</section>
     <section>
-        <div class="controls">
-            <button class="btn btn-dark">Refresh</button>
-            <router-link to="/register">Register as Coach</router-link>
-            <ul v-if="hasCoaches">
-                <li v-for="coach in filteredCoaches" :key="coach.id">
-                    {{coach.firstName}}-{{coach.lastName}}
-                </li>
-            </ul>
-            <h3 v-else>There is no Coaches!!</h3>
+        <div class="container" >
+            <div class="row justify-content-md-center">
+                <div class="col-md-8" id="style_layout">
+                    <button class="btn btn-dark">Refresh</button>
+                    <router-link to="/register">Register as Coach</router-link>
+                    <ul v-if="hasCoaches">
+                        <base-card v-for="coach in filteredCoaches" :key="coach.id">
+                            <h5 class="card-title">Special title treatment</h5>
+                            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </base-card >
+                    </ul>
+                    <h3 v-else>There is no Coaches!!</h3>
+                </div>
+            </div>
         </div>
     </section>
 </template>
@@ -26,3 +32,10 @@ export default {
     }
 }
 </script>
+<style scoped>
+#style_layout{
+    border-radius: 10px;
+    box-shadow: 5px 5px 10px 10px #aaaaaa;
+    padding: 20px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,5 +4,7 @@ import router from './router'
 import store from './store'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import BaseCard from './components/Base/BaseCard.vue'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).component('base-card',BaseCard)
+.use(store).use(router).mount('#app')
